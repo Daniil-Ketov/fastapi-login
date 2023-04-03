ee2cc1cdda19281a596ee287c2cfeab1063d7b6c
Ketov Daniil <ketov.daniil123@yandex.ru>
2023-04-03 14:03:13 +0300

    created frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-yellow-400 flex justify-center items-center">
+      <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20"></div>
+    </div>
+  );
+}
+
+export default App;
